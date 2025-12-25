d1477850bf63a0d4d6e4aa0c913755367558c7cc
Toko Migineishvili <toko.migine@gmail.com>
2025-12-25 22:26:49 +0400

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -6,10 +6,15 @@ const instance = axios.create({
 
 instance.interceptors.request.use((config) => {
      const token = localStorage.getItem('token')
-     if(token && !config.url?.includes("/features/auth/login") &&
-      !config.url?.includes("/features/auth/register")){
-       config.headers.Authorization = `Bearer ${token}`
-     } 
+    if (
+      token &&
+      !config.url?.includes("/auth/login") &&
+      !config.url?.includes("/auth/register")
+    ) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  
+
      return config
 })
 
