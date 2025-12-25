b8709c5a3177d3c71885025852a4294443c362f0
Toko Migineishvili <toko.migine@gmail.com>
2025-12-25 20:52:52 +0400

    Update route.ts

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,6 +3,17 @@ import User from "@/models/user";
 import bcrypt from 'bcryptjs'
 import { NextResponse } from "next/server";
 
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
 interface RegisterForm {
     fullName: string,
     email: string,
