c09a47cea5634b811611a1ad12f802a2a2b12bde
Toko Migineishvili <toko.migine@gmail.com>
2025-12-26 13:47:07 +0400

    n

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://care-book-92s3.vercel.app/api"
+    baseURL: "https://care-book-92s3-cshzd7ukg-toko122s-projects.vercel.app/api"
 })
 
 instance.interceptors.request.use((config) => {
